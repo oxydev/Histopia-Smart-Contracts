e37a47f94376b19127b73bc1b1cd624032bfbf01
hadi esna <45296291+HadiEsna@users.noreply.github.com>
2022-08-15 18:46:20 +0300

    migrate to hardhat

---
+++ contracts/FOE.sol
@@ -0,0 +1,222 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.1;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface INFT {
+    function getCumulativeTokenProperties(uint256) external view returns (uint256[] calldata);
+
+    function transferFrom(address sender, address recipient, uint256 tokenId) external;
+
+    function tokenIdToTypeIndex(uint256 tokenId) external returns (uint256);
+
+}
+interface Allocator{
+    function withdrawShare(address dest, uint256 amount) external;
+    function getEraContractAddress() external returns (address);
+}
+// Fountain of ERA
+//
+// Note that it's ownable and the owner wields tremendous power. The ownership
+// will be transferred to a governance smart contract once ERA is sufficiently
+// distributed and the community can show to govern itself.
+//
+contract FountainOfEra is Ownable {
+    using SafeERC20 for IERC20;
+    // Info of each user.
+    struct UserInfo {
+        uint256 militaryPower; // How many LP tokens the user has provided.
+        uint256 rewardDebt; // Reward debt. See explanation below.
+        uint256[] tokenIDs;
+        //
+        // We do some fancy math here. Basically, any point in time, the amount of ERAs
+        // entitled to a user but is pending to be distributed is:
+        //
+        //   pending reward = (user.militaryPower * generalAccEraPerShare) - user.rewardDebt
+        //
+        // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
+        //   1. The `accEraPerShare` (and `lastRewardBlock`) gets updated.
+        //   2. User receives the pending reward sent to his/her address.
+        //   3. User's `militaryPower` gets updated.
+        //   4. User's `rewardDebt` gets updated.
+    }
+    // Info of each pool.
+    INFT public nftContract; // Address of NFT token contract.
+    Allocator public eraAllocator;
+    IERC20 public era;
+    uint256 public eraPerBlock;
+
+    uint256 public lastRewardBlock; // Last block number that ERAs distribution occurs.
+    uint256 public generalAccEraPerShare; // Accumulated ERAs per share, times 1e12. See below.
+    uint256 public lastBlock;
+    uint256 public currentTotalMilitaryPower;
+
+
+
+    // Info of each user that stakes NFT tokens.
+    mapping(address => UserInfo) public userInfo;
+    mapping(uint256 => bool) public histopianTypes;
+
+
+    event Deposit(address indexed user, uint256[] tokenIds);
+    event Withdraw(address indexed user, uint256 tokenId);
+    event EmergencyWithdraw(
+        address indexed user,
+        uint256[] tokenIds
+    );
+
+    constructor(address _eraAllocatorAddress, address _histopiaNFT, uint256 _eraPerBlock) {
+        eraAllocator = Allocator(_eraAllocatorAddress);
+        eraPerBlock = _eraPerBlock;
+        era = IERC20(eraAllocator.getEraContractAddress());
+        nftContract = INFT(_histopiaNFT);
+    }
+
+    function addHistopianType(uint256 typeId) public onlyOwner{
+        histopianTypes[typeId] = true;
+    }
+
+
+    // Return reward multiplier over the given _from to _to block.
+    function getMultiplier(uint256 _from, uint256 _to)
+        public
+        pure
+        returns (uint256)
+    {
+        return _to - _from;
+    }
+
+    // View function to see pending Eras on frontend.
+    function pendingERA(address _user)
+        external
+        view
+        returns (uint256)
+    {
+        UserInfo storage user = userInfo[_user];
+        uint256 accEraPerShare = generalAccEraPerShare;
+        uint256 militaryRate = currentTotalMilitaryPower;
+        if (block.number > lastRewardBlock && militaryRate != 0) {
+            uint256 multiplier =
+                getMultiplier(lastRewardBlock, block.number);
+            uint256 eraReward = multiplier * eraPerBlock;
+            accEraPerShare += (eraReward * 1e12) / militaryRate;
+        }
+        return ((user.militaryPower * accEraPerShare) / 1e12) - user.rewardDebt;
+    }
+
+    // Update reward variables o be up-to-date.
+    function updatePool() public {
+        if (block.number <= lastRewardBlock) {
+            return;
+        }
+        if (currentTotalMilitaryPower == 0) {
+            lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = getMultiplier(lastRewardBlock, block.number);
+        uint256 eraReward = multiplier * eraPerBlock;
+        eraAllocator.withdrawShare(address(this), eraReward);
+        generalAccEraPerShare += eraReward * 1e12 / currentTotalMilitaryPower;
+        lastRewardBlock = block.number;
+    }
+
+    // Deposit NFT tokens to FOE for Era allocation.
+    function deposit(uint256[] calldata tokenIds) public {
+        UserInfo storage user = userInfo[msg.sender];
+        updatePool();
+        if (user.militaryPower > 0) {
+            uint256 pending = (user.militaryPower * generalAccEraPerShare / 1e12) - user.rewardDebt;
+            safeERATransfer(msg.sender, pending);
+        }
+        uint256 militaryPowerIncreament = 0;
+        for (uint256 index = 0; index < tokenIds.length; index++) {
+            require(histopianTypes[nftContract.tokenIdToTypeIndex(tokenIds[index])], "FOE: Your NFT is not Histopian!.");
+            nftContract.transferFrom(msg.sender, address(this), tokenIds[index]);
+
+            militaryPowerIncreament += calculateMilitaryPowerOfTokenId(tokenIds[index]);
+
+            user.tokenIDs.push(tokenIds[index]);
+        }
+        user.militaryPower += militaryPowerIncreament;
+        currentTotalMilitaryPower += militaryPowerIncreament;
+        user.rewardDebt = user.militaryPower * generalAccEraPerShare / 1e12;
+        emit Deposit(msg.sender, tokenIds);
+    }
+
+    function calculateMilitaryPowerOfTokenId(uint256 tokenId) public view returns (uint256 ) {
+        return calculateMilitaryPower(nftContract.getCumulativeTokenProperties(tokenId));
+    }
+
+    function calculateMilitaryPower(uint256[] memory properties) public pure returns (uint256 militaryPower) {
+        for (uint256 i = 0; i < properties.length; i++) {
+            militaryPower += properties[i];
+        }
+        return militaryPower;
+    }
+
+    // Withdraw LP tokens from MasterChef.
+    function withdraw(uint256[] calldata tokenIndices) public {
+        UserInfo storage user = userInfo[msg.sender];
+        updatePool();
+        uint256 pending = (user.militaryPower * generalAccEraPerShare / 1e12 ) - user.rewardDebt;
+        safeERATransfer(msg.sender, pending);
+        uint256 militaryPowerDecreament;
+        for (uint256 index = 0; index < tokenIndices.length; index++) {
+            require(tokenIndices[index] < user.tokenIDs.length, "FOE: Invalid token index");
+            emit Withdraw(msg.sender, user.tokenIDs[tokenIndices[index]]);
+            nftContract.transferFrom(address(this), msg.sender, user.tokenIDs[tokenIndices[index]]);
+            militaryPowerDecreament += calculateMilitaryPowerOfTokenId(user.tokenIDs[tokenIndices[index]]);
+            user.tokenIDs[tokenIndices[index]] = 0;
+        }
+        user.militaryPower -= militaryPowerDecreament;
+        currentTotalMilitaryPower -= militaryPowerDecreament;
+        for (uint256 index = tokenIndices.length; index > 0; index--) {
+            uint256 j = index - 1;
+            if (tokenIndices[j]  == user.tokenIDs.length - 1) {
+                user.tokenIDs.pop();
+            }
+            else if (tokenIndices[j] < user.tokenIDs.length - 1) {
+                uint256 movingTokenId = user.tokenIDs[user.tokenIDs.length - 1];
+                while (movingTokenId == 0 && user.tokenIDs.length > 0) {
+                    user.tokenIDs.pop();
+                    movingTokenId = user.tokenIDs[user.tokenIDs.length - 1];
+                } 
+                if (user.tokenIDs.length == 0) {
+                    break;
+                }
+                user.tokenIDs[tokenIndices[j]] = movingTokenId;
+                user.tokenIDs.pop();
+            }
+        }
+        user.rewardDebt = user.militaryPower * generalAccEraPerShare / 1e12;
+    }
+
+    // Withdraw without caring about rewards. EMERGENCY ONLY.
+    function emergencyWithdraw() public {
+        UserInfo storage user = userInfo[msg.sender];
+        for (uint256 index = 0; index < user.tokenIDs.length; index++) {
+            nftContract.transferFrom(address(this), msg.sender, user.tokenIDs[index]);
+        }
+        emit EmergencyWithdraw(msg.sender, user.tokenIDs);
+        delete user.tokenIDs;
+        user.rewardDebt = 0;
+        user.militaryPower = 0;
+    }
+
+    // Safe ERA transfer function, just in case if rounding error causes pool to not have enough ERAs.
+    function safeERATransfer(address _to, uint256 _amount) internal {
+        uint256 eraBal = era.balanceOf(address(this));
+        if (_amount > eraBal) {
+            era.transfer(_to, eraBal);
+        } else {
+            era.transfer(_to, _amount);
+        }
+    }
+
+    function getTokenIds(address user) public view returns (uint256[] memory) {
+        return userInfo[user].tokenIDs;
+    }
+}
---
+++ test/FOE_tests.ts
@@ -0,0 +1,309 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("FOE", function () {
+    // We define a fixture to reuse the same setup in every test.
+    // We use loadFixture to run this setup once, snapshot that state,
+    // and reset Hardhat Network to that snapshopt in every test.
+    async function deployEraAndNFT() {
+
+        // Contracts are deployed using the first signer/account by default
+        const [owner, otherAccount] = await ethers.getSigners();
+        const ERA = await ethers.getContractFactory("ERA");
+        const era = await ERA.deploy();
+
+        const NFT = await ethers.getContractFactory("AttachableERC721");
+        const nft = await NFT.deploy("NFT", "NFT", era.address, 0);
+        await nft.addType(
+            "Histopian",
+            30,
+            100,
+            ["speed", "strength", "intelligence", "charisma", "luck"],
+            [10, 10, 10, 10, 10],
+            [100, 100, 100, 100, 100],
+        )
+        for (let index = 0; index < 5; index++) {
+            await nft.mint(owner.address, 0);   
+        }
+        for (let index = 0; index < 5; index++) {
+            await nft.mint(otherAccount.address, 0);   
+        }
+        return { era, nft, owner, otherAccount };
+    }
+
+    async function deployFOE() {
+
+        // Contracts are deployed using the first signer/account by default
+        const {era, nft, owner, otherAccount} = await deployEraAndNFT();
+
+        const FOE = await ethers.getContractFactory("FountainOfEra");
+        const foe = await FOE.deploy(era.address, nft.address, 1000);
+
+        return { era, nft, foe,  owner, otherAccount };
+    }
+
+    describe("Deployment", function () {
+        it("Should set the right nft contract address", async function () {
+            const { era, nft, foe,  owner, otherAccount  } = await loadFixture(deployFOE);
+
+            expect(await nft.balanceOf(owner.address)).to.equal(5);
+            expect(await nft.balanceOf(otherAccount.address)).to.equal(5);
+
+            let nftPropertiesArr = [];
+            for (let index = 1; index < 6; index++) {
+                let nftProperties = await nft.getCumulativeTokenProperties(index)
+                // console.log("nft Properties : ", index,  nftProperties)
+                let nftPower = 0;
+                for (let index = 0; index < nftProperties.length; index++) {
+                    const element = nftProperties[index];
+                    nftPower += Number(element);
+                }
+                nftPropertiesArr.push(nftPower);
+            }
+            // console.log("nftPropertiesArr : ", nftPropertiesArr)
+            
+            await nft.setApprovalForAll(foe.address, true);
+            // await nft.setApprovalForAll(foe.address, true, {from: otherAccount.address});
+            await foe.addHistopianType(0);
+            await foe.deposit([1,2,3]);
+
+            expect(await nft.balanceOf(owner.address)).to.equal(2);
+            expect(await nft.balanceOf(foe.address)).to.equal(3);
+
+            expect(await foe.era()).to.equal(era.address);
+            expect(await foe.eraAllocator()).to.equal(era.address);
+
+        });
+    });
+
+    describe("Deposit and withdraw", function () {
+        it("Should set the deposit", async function () {
+            const { era, nft, foe,  owner, otherAccount  } = await loadFixture(deployFOE);
+
+            expect(await nft.balanceOf(owner.address)).to.equal(5);
+            expect(await nft.balanceOf(otherAccount.address)).to.equal(5);
+            
+            let nftPropertiesArr = [];
+            for (let index = 1; index < 6; index++) {
+                let nftProperties = await nft.getCumulativeTokenProperties(index)
+                // console.log("nft Properties : ", index,  nftProperties)
+                let nftPower = 0;
+                for (let index = 0; index < nftProperties.length; index++) {
+                    const element = nftProperties[index];
+                    nftPower += Number(element);
+                }
+                nftPropertiesArr.push(nftPower);
+            }
+            // console.log("nftPropertiesArr : ", nftPropertiesArr)
+            let ownerTotalPower = nftPropertiesArr[0] + nftPropertiesArr[1] + nftPropertiesArr[2];
+            await nft.setApprovalForAll(foe.address, true);
+            await foe.addHistopianType(0);
+            await foe.deposit([1,2,3]);
+            
+            expect(await nft.balanceOf(owner.address)).to.equal(2);
+            expect(await nft.balanceOf(foe.address)).to.equal(3);
+
+            expect(await foe.currentTotalMilitaryPower()).to.equal(ownerTotalPower);
+            expect((await foe.userInfo(owner.address)).militaryPower).to.equal(ownerTotalPower);
+
+            await nft.connect(otherAccount).setApprovalForAll(foe.address, true);
+            await foe.connect(otherAccount).deposit([6,7,8]);
+            let otherAccountTotalPower = 0
+            for (let index = 6; index < 9; index++) {
+                let nftPower = await await foe.calculateMilitaryPowerOfTokenId(index)
+                // console.log("nft Properties : ", index,  nftProperties)
+
+                otherAccountTotalPower += Number(nftPower);
+            }
+
+            expect(await foe.currentTotalMilitaryPower()).to.equal(ownerTotalPower + otherAccountTotalPower);
+            expect(await (await foe.userInfo(otherAccount.address)).militaryPower).to.equal(otherAccountTotalPower);
+        });
+
+        it("Should set the deposit and withdraw correctly", async function () {
+            const { era, nft, foe,  owner, otherAccount  } = await loadFixture(deployFOE);
+    
+            expect(await nft.balanceOf(owner.address)).to.equal(5);
+            expect(await nft.balanceOf(otherAccount.address)).to.equal(5);
+            
+            let nftPropertiesArr = [];
+            for (let index = 1; index < 6; index++) {
+                let nftProperties = await nft.getCumulativeTokenProperties(index)
+                // console.log("nft Properties : ", index,  nftProperties)
+                let nftPower = 0;
+                for (let index = 0; index < nftProperties.length; index++) {
+                    const element = nftProperties[index];
+                    nftPower += Number(element);
+                }
+                nftPropertiesArr.push(nftPower);
+            }
+            // console.log("nftPropertiesArr : ", nftPropertiesArr)
+            let ownerTotalPower = nftPropertiesArr[0] + nftPropertiesArr[1] + nftPropertiesArr[2];
+            await nft.setApprovalForAll(foe.address, true);
+            await foe.addHistopianType(0);
+            await foe.deposit([1,2,3]);
+            
+            expect(await nft.balanceOf(owner.address)).to.equal(2);
+            expect(await nft.balanceOf(foe.address)).to.equal(3);
+    
+            expect(await foe.currentTotalMilitaryPower()).to.equal(ownerTotalPower);
+            expect((await foe.userInfo(owner.address)).militaryPower).to.equal(ownerTotalPower);
+    
+            await nft.connect(otherAccount).setApprovalForAll(foe.address, true);
+            await foe.connect(otherAccount).deposit([6,7,8,9,10]);
+            let otherAccountTotalPower = 0
+            for (let index = 6; index < 11; index++) {
+                let nftPower = await await foe.calculateMilitaryPowerOfTokenId(index)
+                // console.log("nft Properties : ", index,  nftProperties)
+    
+                otherAccountTotalPower += Number(nftPower);
+            }
+    
+            expect(await foe.currentTotalMilitaryPower()).to.equal(ownerTotalPower + otherAccountTotalPower);
+            expect((await foe.userInfo(otherAccount.address)).militaryPower).to.equal(otherAccountTotalPower);
+    
+    
+            await foe.withdraw([0,1,2]);
+    
+            expect(await foe.currentTotalMilitaryPower()).to.equal(otherAccountTotalPower);
+            expect((await foe.userInfo(owner.address)).militaryPower).to.equal(0);
+    
+    
+            // console.log("otherAccount deposited NFTs : ", await foe.getTokenIds(otherAccount.address));
+            await foe.connect(otherAccount).withdraw([1]);
+            // console.log("otherAccount deposited NFTs : ", await foe.getTokenIds(otherAccount.address));
+            await foe.connect(otherAccount).withdraw([3]);
+            // console.log("otherAccount deposited NFTs : ", await foe.getTokenIds(otherAccount.address));
+            await foe.connect(otherAccount).withdraw([0]);
+            // console.log("otherAccount deposited NFTs : ", await foe.getTokenIds(otherAccount.address));
+            otherAccountTotalPower = 0
+    
+            otherAccountTotalPower += Number(await foe.calculateMilitaryPowerOfTokenId(8));
+            otherAccountTotalPower += Number(await foe.calculateMilitaryPowerOfTokenId(10));
+            expect(await foe.currentTotalMilitaryPower()).to.equal(otherAccountTotalPower);
+            expect((await foe.userInfo(otherAccount.address)).militaryPower).to.equal(otherAccountTotalPower);
+    
+        });
+    });
+
+    describe("Reward", function () {
+        it("Should send the reward for one participant", async function () {
+            const { era, nft, foe,  owner, otherAccount  } = await loadFixture(deployFOE);
+
+            expect(await nft.balanceOf(owner.address)).to.equal(5);
+            expect(await nft.balanceOf(otherAccount.address)).to.equal(5);
+            
+            let nftPropertiesArr = [];
+            for (let index = 1; index < 6; index++) {
+                let nftProperties = await nft.getCumulativeTokenProperties(index)
+                // console.log("nft Properties : ", index,  nftProperties)
+                let nftPower = 0;
+                for (let index = 0; index < nftProperties.length; index++) {
+                    const element = nftProperties[index];
+                    nftPower += Number(element);
+                }
+                nftPropertiesArr.push(nftPower);
+            }
+            // console.log("nftPropertiesArr : ", nftPropertiesArr)
+            let ownerTotalPower = nftPropertiesArr[0] + nftPropertiesArr[1] + nftPropertiesArr[2];
+            await nft.setApprovalForAll(foe.address, true);
+            await foe.addHistopianType(0);
+            await foe.deposit([1,2,3]);
+            
+            let currentBlock = await time.latestBlock();
+
+            expect(await nft.balanceOf(owner.address)).to.equal(2);
+            expect(await nft.balanceOf(foe.address)).to.equal(3);
+
+            expect(await foe.currentTotalMilitaryPower()).to.equal(ownerTotalPower);
+            expect((await foe.userInfo(owner.address)).militaryPower).to.equal(ownerTotalPower);
+
+            let currentTime = await time.latest()
+            let calculatedReward = await foe.pendingERA(owner.address);
+            expect(calculatedReward).to.equal(0);
+            for (let index = 0; index < 100; index++) {
+                await time.increaseTo(currentTime + 86400);  
+                currentTime = await time.latest();       
+            }
+            let elapsedBlocks = await time.latestBlock() - currentBlock;
+            calculatedReward = await foe.pendingERA(owner.address);
+            expect(calculatedReward).to.equal(elapsedBlocks * 1000 - 1);
+            await foe.withdraw([0,1,2]);
+            expect(await foe.currentTotalMilitaryPower()).to.equal(0);
+            elapsedBlocks = await time.latestBlock() - currentBlock;
+            expect((await era.balanceOf(owner.address))).to.equal(elapsedBlocks * 1000 - 1);
+        });
+
+        it("Should set the deposit and withdraw the reward correctly for two participants", async function () {
+            const { era, nft, foe,  owner, otherAccount  } = await loadFixture(deployFOE);
+    
+            expect(await nft.balanceOf(owner.address)).to.equal(5);
+            expect(await nft.balanceOf(otherAccount.address)).to.equal(5);
+            
+            let nftPropertiesArr = [];
+            for (let index = 1; index < 6; index++) {
+                let nftProperties = await nft.getCumulativeTokenProperties(index)
+                // console.log("nft Properties : ", index,  nftProperties)
+                let nftPower = 0;
+                for (let index = 0; index < nftProperties.length; index++) {
+                    const element = nftProperties[index];
+                    nftPower += Number(element);
+                }
+                nftPropertiesArr.push(nftPower);
+            }
+            // console.log("nftPropertiesArr : ", nftPropertiesArr)
+            let ownerTotalPower = nftPropertiesArr[0] + nftPropertiesArr[1] + nftPropertiesArr[2];
+            await nft.setApprovalForAll(foe.address, true);
+            await foe.addHistopianType(0);
+            await foe.deposit([1,2,3]);
+            let currentBlock = await time.latestBlock();
+
+            let currentTime = await time.latest()
+            for (let index = 0; index < 100; index++) {
+                await time.increaseTo(currentTime + 86400);  
+                currentTime = await time.latest();       
+            }
+
+            let elapsedBlocks = await time.latestBlock() - currentBlock;
+            let calculatedRewardPhase1 = Number(await foe.pendingERA(owner.address));
+            expect(calculatedRewardPhase1).to.equal(elapsedBlocks * 1000 - 1);
+
+            await nft.connect(otherAccount).setApprovalForAll(foe.address, true);
+            await foe.connect(otherAccount).deposit([6,7,8,9,10]);
+
+            elapsedBlocks = await time.latestBlock() - currentBlock;
+            calculatedRewardPhase1 = Number(await foe.pendingERA(owner.address));
+            expect(calculatedRewardPhase1).to.equal(elapsedBlocks * 1000 - 1);
+
+            let otherAccountStartBlock = await time.latestBlock();
+            let otherAccountTotalPower = Number((await foe.userInfo(otherAccount.address)).militaryPower);
+   
+
+            for (let index = 0; index < 100; index++) {
+                await time.increaseTo(currentTime + 86400);  
+                currentTime = await time.latest();       
+            }
+
+            console.log("elapsedBlocks : ", elapsedBlocks)
+            elapsedBlocks = await time.latestBlock() - otherAccountStartBlock;
+            let calculatedRewardPhase2owner = await foe.pendingERA(owner.address);
+            let calculatedRewardPhase2other = await foe.pendingERA(otherAccount.address);
+            console.log("calculatedRewardPhase1owner : ", calculatedRewardPhase1);
+            console.log("owner power", (await foe.userInfo(owner.address)).militaryPower);
+            console.log("calculatedRewardPhase2owner : ", calculatedRewardPhase2owner);
+            console.log("other power", (await foe.userInfo(otherAccount.address)).militaryPower);
+            console.log("calculatedRewardPhase2other : ", calculatedRewardPhase2other);
+            console.log("elapsedBlocks : ", elapsedBlocks);    
+            console.log("calculatedRewardPhase2other : ", (elapsedBlocks * 1000 * otherAccountTotalPower / (ownerTotalPower + otherAccountTotalPower)),
+            Math.round(elapsedBlocks * 1000 * otherAccountTotalPower / (ownerTotalPower + otherAccountTotalPower))
+            );
+            
+            expect(calculatedRewardPhase2other).to.equal(Math.round(elapsedBlocks * 1000 * otherAccountTotalPower / (ownerTotalPower + otherAccountTotalPower)));    
+            expect(calculatedRewardPhase2owner).to.equal(Math.ceil((elapsedBlocks * 1000 * ownerTotalPower / (ownerTotalPower + otherAccountTotalPower)) + calculatedRewardPhase1));    
+        });
+    });
+
+
+});
