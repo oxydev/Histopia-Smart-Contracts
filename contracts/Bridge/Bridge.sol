35f976f48b2844beb1d54cbb370807e1450c1c26
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2022-09-25 01:38:30 +0300

    Bridge contract fee collection

--- contracts/Bridge/Bridge.sol
+++ contracts/Bridge/Bridge.sol
@@ -4,17 +4,20 @@ import "../ERA.sol";
 
 contract BridgeERA {
     uint256 public MINIMUM_THRESHOLD = 10 ** 18; // 1 ERA
+    address public feeCollector;
     ERA public ERAContract;
     event Locked(uint256 amount, address indexed to, uint256 indexed destChain);
 
-    constructor(ERA _ERA) {
+    constructor(ERA _ERA, address _feeCollector) {
         ERAContract = _ERA;
+        feeCollector = _feeCollector;
     }
 
     // Locks ERA tokens in the contract and backend mints ERA token on the other chain
     function lock(uint256 amount, address to, uint256 destChain) public {
         require(amount > MINIMUM_THRESHOLD, "Amount must be greater than Minimum Threshold");
-        ERAContract.burn(msg.sender, amount);
-        emit Locked(amount, to, destChain);
+        ERAContract.burn(msg.sender, amount * 99 / 100);
+        ERAContract.transferFrom(msg.sender, amount / 100);
+        emit Locked(amount * 99 / 100, to, destChain);
     }
 }
